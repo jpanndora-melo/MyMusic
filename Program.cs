05be8abe1ea005342b9f3a5ac5814567280cc96b
Jessica <jpanndora@gmail.com>
2025-09-14 18:42:04 -0300

    Atividade Concluida

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("A Thousand Years- Christina Perri"); 
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Heart beats fast");
+Console.WriteLine("Colors and promises");
+Console.WriteLine("How to be brave?");
+Console.WriteLine("How can I love when I'm afraid to fall?");
+Console.WriteLine("But watching you stand alone"); 
+Console.WriteLine("All of my doubt suddenly goes away somehow");
+Console.WriteLine("One step closer");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I have died every day waiting for you");
+Console.WriteLine("Darling don't be afraid");
+Console.WriteLine("I have loved you for a thousand years");
+Console.WriteLine("I'll love you for a thousand more");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Time stands still");
+Console.WriteLine("Beauty in all she is");
+Console.WriteLine("I will be brave");
+Console.WriteLine("I will not let anything take away");
+Console.WriteLine("What's standing in front of me");
+Console.WriteLine("Every breath");
+Console.WriteLine("Every hour has come to this");
+Console.WriteLine("One step closer");
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I have died every day waiting for you");
+Console.WriteLine("Darling don't be afraid");
+Console.WriteLine("I have loved you for a thousand years");
+Console.WriteLine("I'll love you for a thousand more");
+Console.WriteLine("And all along I believed I would find you");
+Console.WriteLine("Time has brought your heart to me");
+Console.WriteLine("I have loved you for a thousand years");
+Console.WriteLine("I'll love you for a thousand more");  
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("One step closer");
+Console.WriteLine("One step closer");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("I have died every day waiting for you");
+Console.WriteLine("Darling don't be afraid");
+Console.WriteLine("For a thousand years");
+Console.WriteLine("I'll love you for a thousand more");
+Console.WriteLine("And all along I believed I would find you");
+Console.WriteLine("Time has brought your heart to me");
+Console.WriteLine("I have loved you for a thousand years");
+Console.WriteLine("I'll love you for a thousand more");
+Console.ResetColor();
+Console.WriteLine();
